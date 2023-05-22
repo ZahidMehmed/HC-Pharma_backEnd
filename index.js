e7ba6abb1d79488f8a235f9667b3196090e356bf
Zahid Mehmood <chzahidm.431@gmail.com>
2023-05-22 21:52:17 +0500

    updated

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const bodyParser = require('body-parser')
 app.use(`/uploads`, express.static('./uploads'))
 app.use(express.json())
 const cors = require('cors')
+app.use(cors(
+  {
+    origin:"https://hcp-harma.vercel.app",
+    methods:["GET", "POST", "PUT", "DELETE"]
+  }
+))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -18,12 +24,6 @@ const ConsDetails = require('./ScheemaModels/ConsultantScheema')
 const PORT = process.env.PORT || 350
 // app.use(`/uploads`, express.static('./uploads'))
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://hcp-harma.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
 
 
 

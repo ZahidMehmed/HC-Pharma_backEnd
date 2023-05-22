f86a812d13807f87041dc81583587798366a2d96
Zahid Mehmood <chzahidm.431@gmail.com>
2023-05-22 21:46:45 +0500

    updated

--- index.js
+++ index.js
@@ -17,12 +17,7 @@ app.use(
 const ConsDetails = require('./ScheemaModels/ConsultantScheema')
 const PORT = process.env.PORT || 350
 // app.use(`/uploads`, express.static('./uploads'))
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://hcp-harma.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://hcp-harma.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');

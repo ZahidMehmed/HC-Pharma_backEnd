4d64b48eee18e3b502f4402acefed46698c1b0a3
Zahid Mehmood <chzahidm.431@gmail.com>
2023-05-24 19:00:27 +0500

    updated

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 350
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 const {
   EmployePostRouter,
   EmployeGetRouter,

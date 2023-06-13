85533e8a7d5368ca6a248084e47f24e5dd800f69
Zahid Mehmood <chzahidm.431@gmail.com>
2023-06-13 21:25:58 +0500

    updated

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 350;
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers",
+      "Origin, X-Rquested-With, Content-Type, Accept");
   next();
 });
 

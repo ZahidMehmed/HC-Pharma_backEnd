cc1c96e980a9f3c27c583cbd907b0ee411cf1775
Zahid Mehmood <chzahidm.431@gmail.com>
2023-06-13 21:30:01 +0500

    updated

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  response.header("Access-Control-Allow-Headers",
-        "Origin, X-Rquested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers","Origin, X-Rquested-With, Content-Type, Accept");
   next();
 });
 

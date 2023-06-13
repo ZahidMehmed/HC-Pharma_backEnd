f1f7e4c3cc50c9b268ea6735875676a338307b13
Zahid Mehmood <chzahidm.431@gmail.com>
2023-06-13 21:36:39 +0500

    updated

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.header("Access-Control-Allow-Headers","Origin, X-Rquested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Expose-Headers', 'Content-Length');
   next();
 });
 

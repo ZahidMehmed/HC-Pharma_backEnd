359540096c0158ef5c6c426ee4e9756cbbefcf6e
Zahid Mehmood <chzahidm.431@gmail.com>
2023-06-13 21:08:46 +0500

    updated

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const mongoose =  require('mongoose');
-// mongoose.connect(`mongodb+srv://chzahidm431:7t6zdtzMX2CSQYN9@cluster0.fgehbfn.mongodb.net/`);
-mongoose.connect('mongodb://0.0.0.0:27017/HC-Pharma');
+mongoose.connect(`mongodb+srv://chzahidm431:7t6zdtzMX2CSQYN9@cluster0.fgehbfn.mongodb.net/`);
+// mongoose.connect('mongodb://0.0.0.0:27017/HC-Pharma');
 const dotenv =  require('dotenv');
 dotenv.config();
 mongoose.set('strictQuery', false);
